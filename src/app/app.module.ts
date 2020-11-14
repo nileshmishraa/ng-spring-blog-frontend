3a757e8975b29bfc8e5b2fb03634803cb660551c
Nilesh <cnilesh97@gmail.com>
2020-11-14 17:19:25 +0530

    Register Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LloginComponent } from './auth/login/llogin.component';
+import { RegisterSuccessComponent } from './auth/register-success/register-success.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RegisterComponent,
+    LloginComponent,
+    RegisterSuccessComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {path: 'register', component: RegisterComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
